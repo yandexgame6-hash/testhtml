fd4899c8926cc58977460ceb00c77790d49c80da
yandexgame6-hash <yandexgame6@gmail.com>
2026-07-04 00:08:20 +0100

    Add files via upload

---
+++ Pop.js
@@ -0,0 +1,9 @@
+  window.CPA_CONFIG = {
+     "enabled": true,
+     "autoStart": "On",
+     "link": "https://app.hawktrk.com/sl?id=6a2050db46d3cf0d62f32aa4&pid=2&sub2=u787576&sub6=s2smartLink&sub5=s1SUBID1HERE",
+     "image": "https://i.ytimg.com/vi/WnVKl_ICewc/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDK_tocNQf8SYAXTMyLJ6b4u6ZOrg",
+     "ctaText": "GET IT NOW",
+     "closeAfter": 6,
+     "urgencySeconds": 600
+   };
